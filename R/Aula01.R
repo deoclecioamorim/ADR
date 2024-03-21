7b4bfc9e3eefa5fadf8332b254d44ba6f40a6e96
deoclecioamorim <deocleciojardim@hotmail.com>
2024-03-21 10:48:55 -0300

    Atualizacao 1

---
+++ R/Aula01.R
@@ -0,0 +1,99 @@
+###############################################################################
+####### Análise de dados com o R (ADR) ########################################
+####### Deoclecio Jardim Amorim  ##############################################
+####### Eduardo Mariano          ##############################################
+###############################################################################
+
+#Link do curso
+"https://uspdigital.usp.br/apolo/apoObterCurso?cod_curso=640400020&cod_edicao=24001&numseqofeedi=1"
+
+# Principais operações aritméticas e lógicas com o R --------------------------------------
+#'
+#' Operador | Descrição
+#' ---------|----------
+#' +        | adição
+#' -        | subtração
+#' *        | multiplicação
+#' /        | divisão
+#' :        | sequência
+#' ^        | exponecial
+#' <        | menor que
+#' <=       | menor ou igual
+#' >        | maior que
+#' >=       | maior ou igual
+#' ==       | igual
+#' !=       | diferente
+#' !        | não
+#' \|       | ou
+#' &        | e
+#' %in%     | pertence
+#'
+## Exemplos
+#'
+#Soma
+2+2
+
+#Subrtração
+2-2
+
+#Divisão
+2/2
+
+#Operador lógico
+#Maior
+3>4
+
+#Menor
+3<4
+
+#igualdade
+3==4
+
+# Funções especiais ---------------------------------------------------------------------------
+
+#Número pi= 3,14...
+pi
+
+#função sin()
+sin(pi)
+
+#função cos()
+cos(pi)
+
+#função log() e exp()
+
+log(0)
+
+log(1)
+
+exp(1)
+
+log(exp(1))
+
+#log na base 10
+log10(100)
+
+# Preciso de socorro --------------------------------------------------------------------------
+
+?log()
+
+
+# Objetos -------------------------------------------------------------------------------------
+
+#'Tipos comuns
+#'
+#'-Vetores
+#'-Matrizes
+#'-dataframes
+#'-listas
+#'-funções
+#'
+
+
+
+
+
+
+
+
+
